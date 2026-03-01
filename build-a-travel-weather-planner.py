d8302b86d81f504f8994323a19fc87bfe1f3924b
Unabia Sahar <unabiasahar4@gmail.com>
2026-03-01 17:06:33 +0500

    Add files via upload

---
+++ build-a-travel-weather-planner.py
@@ -0,0 +1,27 @@
+distance_mi = 3
+is_raining = True
+has_bike = True
+has_car = True
+has_ride_share_app = True
+
+
+if distance_mi == False:
+    print("False")
+
+elif distance_mi<=1 and is_raining == False:
+    print('True')
+
+elif distance_mi>1 and distance_mi<=6 and is_raining == False and has_bike== True:
+    print('True')
+
+elif distance_mi>1 and distance_mi<=6 and is_raining == True and has_bike== False:
+    print('False')
+
+elif distance_mi>1 and distance_mi<=6 and is_raining == False and has_bike== False:
+    print('False')
+
+elif distance_mi>6 and has_car == True or has_ride_share_app == True:
+    print('True')
+    
+else:
+    print('False')
